cfe3168e06f9f975bca9096e6fc1427611470284
Grace Braden <gracebraden97@gmail.com>
2018-01-31 22:47:39 -0600

    Displays welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\n\nHello, PasswordManagerApp!\n");
+        //System.out.println("\n\nHello, PasswordManagerApp!\n");
+        System.out.println("\n\nWelcome to the PasswordManager!!\n");
+        
     }
 }
